db05ebf200ca3009a2fe64b17b269a4a207cd2b2
Valentine <valentinemaillard1@gmail.com>
2020-01-06 17:26:28 -0500

    Done.

--- index.js
+++ index.js
@@ -8,4 +8,20 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time){
+  const parsed = parseInt(time)
+  if (parsed < 12){
+    return "Good Morning"
+  } else if (parsed > 17 ){
+    return "Good Evening"
+  }else{
+    return "Good Afternoon"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+  let greeting = document.querySelector('h1#greeting')
+  greeting.innerText = string
+}
